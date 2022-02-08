dc0db8ff21a82a80e37ef5469d55701d86a79369
YoshinoriSatoh <yoshinori.satoh.tokyo@gmail.com>
2022-02-08 19:06:30 +0900

    up

--- outputs.tf
+++ outputs.tf
@@ -2,14 +2,14 @@ output "security_group_id" {
   value = aws_security_group.ecs_service.id
 }
 
-output "task-role" {
+output "task_role" {
   value = aws_iam_role.task-role
 }
 
-output "task-execution-role" {
+output "task_execution_role" {
   value = aws_iam_role.task-execution-role
 }
 
-output "ecs-service-name" {
+output "ecs_service_name" {
   value = local.service.fullname
 }
